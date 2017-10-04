5d33ef4e76a161310bb6c51a9d548dd7ee957bf5
Nityan Bhorde <bhordenityan@hotmail.com>
2017-10-03 23:54:09 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ router.get('/contests',(req,res) => {
 
 router.get('/contests/:contestId',(req,res) => {
 	let contest = contests[req.params.contestId];
-	contest.description = 'hipitty'
+	contest.description = 'test'
 	res.send(contest);
 });
 
